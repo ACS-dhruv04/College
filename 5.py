c146bf77ae6545a4091efc0e34bd64f6828469f9
ACS-dhruv04 <dhruvil1304@gmail.com>
2025-07-26 08:29:50 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,23 @@
+student = {'student1': 
+                {'name': 'Ariana', 'age': 20, 'grade': 'A'},
+        'student2': 
+                {'name': 'Naman', 'age': 20, 'grade': 'A+'},
+        'student3': 
+                {'name': 'Hamzah', 'age': 19, 'grade': 'B+'}}
+
+# Accessing elements
+print("Details of Student 1 :")
+print("Name:", student['student1']['name'])
+print("Age:", student['student1']['age'])
+print("Grade:", student['student1']['grade'])
+print("-----------------------------------------------")
+print("Details of Student 2 :")
+print("Name:", student['student2']['name'])
+print("Age:", student['student2']['age'])
+print("Grade:", student['student2']['grade'])
+print("-----------------------------------------------")
+print("Details of Student 3 :")
+print("Name:", student['student3']['name'])
+print("Age:", student['student3']['age'])
+print("Grade:", student['student3']['grade'])
+print("-----------------------------------------------")
